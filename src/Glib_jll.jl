82780a5b669a627d9837fde6ad773cf1abe6c1fc
jlbuild <juliabuildbot@gmail.com>
2025-12-01 17:22:26 +0000

    Glib_jll build 2.86.2+0

--- src/Glib_jll.jl
+++ src/Glib_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Glib")
-JLLWrappers.@generate_main_file("Glib", UUID("7746bdde-850d-59dc-9ae8-88ece973131d"))
+JLLWrappers.@generate_main_file("Glib", Base.UUID("7746bdde-850d-59dc-9ae8-88ece973131d"))
 end  # module Glib_jll
